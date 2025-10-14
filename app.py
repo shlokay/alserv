fe04156c5ccb630ca9ac90542e71a9e00edd073a
Shlok Khubchandani <127543765+shlokay@users.noreply.github.com>
2025-10-14 13:22:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 
 st.set_page_config(page_title="Service Report Generator", page_icon="🧾", layout="centered")
 
-st.title("🧾 Filtered Service Report Generator")
+st.title("🧾 Service History Generator (by YASH MOTORS)")
 
 uploaded_file = st.file_uploader("Upload Excel File (.xlsx)", type=["xlsx"])
 
